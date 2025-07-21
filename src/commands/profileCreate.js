dff9801044b38ba8c1387a06d326401f4e453a66
Greg Priday <greg@siteorigin.com>
2025-07-21 07:01:36 +0200

    Remove profile creation file
    - Clean up unnecessary profile creation functionality
    - Simplify codebase structure
    - Remove redundant code
    - Prepare for cleaner implementation

--- src/commands/profileCreate.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = async function profileCreateCommand(path, options) {
-  console.log(chalk.blue(`Creating profile for: ${path}`));
-  console.log(chalk.gray('Character limit:'), options.charLimit);
-  console.log(chalk.yellow('\nThis command is not yet implemented.'));
-};
